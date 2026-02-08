30673178d120f60b966ec5044e1ac421da2fa3ef
slipclotmc <slipclotmc8@gmail.com>
2026-02-08 00:01:18 -0600

    Add initial implementation of shopping cart program

---
+++ carrito.c
@@ -0,0 +1,25 @@
+/*Velazquez Dominguez Ricardo
+    Practica I EDA I*/
+
+#include <stdio.h>
+
+int main() {
+    
+  int n1, n2, n3, n4, n5, p1, p2, p3, p4, p5, costo, opcion;
+  
+  printf("**Papelería**\n");
+  printf("Productos:\n");
+  printf("1 •Cuaderno 200 hojas rayado - Precio: $180 - Disponibles: 30\n");
+  printf("2 •Pluma Azor Pin Point Punto Fino 0.7 Mm paquete con 12 Piezas Tinta Azul - Precio: $81 - Disponibles: 25\n");
+  printf("3 •Hoja Tamaño Carta Paq 500 Scribe Impresión Color Blanco - Precio: $125 - Disponibles: 20\n");
+  printf("4 •Notas Post-it Cuadradas 12 Colores, Pack 1200 Notas 7.6x7.6 cm - Precio: $214 - Disponibles: 27\n");
+  printf("5 •Lápices De Colores Prismacolor Con 24 Doble Punta 48 Colores - Precio: $269 - Disponibles: 15\n");
+  printf(" ")
+  
+  switch (opcion)
+   
+ 
+  
+  
+    return 0;
+}
